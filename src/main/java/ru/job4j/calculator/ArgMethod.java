0f3c7c25e3f16ec2da3596ac6cc48d805599ff9b
Larvixx <weselov@list.ru>
2023-04-05 14:03:26 +0300

    Andrey

--- src/main/java/ru/job4j/calculator/ArgMethod.java
+++ src/main/java/ru/job4j/calculator/ArgMethod.java
@@ -7,7 +7,7 @@ public class ArgMethod {
     }
 
     public static void main(String[] args) {
-        String name = "Job4j";
+        String name = "Job4";
         int age = 6;
         ArgMethod.hello(name, age);
         ArgMethod.hello(name, age);
