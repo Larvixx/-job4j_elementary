7ea1e208cd17a71c6b76a783aeb8e9b231ffaadd
Larvixx <weselov@list.ru>
2023-04-07 16:05:27 +0300

    4.3.4. if c return [#333785]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -1,7 +1,6 @@
 package ru.job4j.converter;
 
 public class Converter {
-
     public static float rubleToEuro(float value) {
         float rsl = value / 70;
         return rsl;
@@ -14,9 +13,8 @@ public class Converter {
 
     public static void main(String[] args) {
         float euro = Converter.rubleToEuro(140);
-        float dollar = Converter.rubleToDollar(140);
+        float usd = Converter.rubleToDollar(140);
         System.out.println("140 rubles are " + euro + " euro.");
-        System.out.println("140 rubles are " + dollar + " dollars.");
+        System.out.println("140 rubles are " + usd + "usd");
     }
-
 }
