342c2bf60e21603ccb753093c7b7d99e193b7898
Andrey <weselov@list.ru>
2023-04-13 15:31:00 +0300

    5.7. Ипотека [#156315]

--- src/main/java/ru/job4j/condition/SqArea.java
+++ src/main/java/ru/job4j/condition/SqArea.java
@@ -3,9 +3,8 @@ package ru.job4j.condition;
 public class SqArea {
     public static double square(double p, double k) {
         double h = p / (2 * (k + 1));
-        double w = k * h;
-        double s = w * h;
-        return s;
+        double l = h * k;
+        return l * h;
     }
 
     public static void main(String[] args) {
