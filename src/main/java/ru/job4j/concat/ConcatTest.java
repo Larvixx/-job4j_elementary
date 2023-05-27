ebd833fa4a437d73c577422711c0c8f75a5df03b
Andrey Weselov <weselov@list.ru>
2023-05-27 19:25:41 +0300

    3. Тестовое задание. Блок "Базовый синтаксис" [#505049]

--- src/main/java/ru/job4j/concat/ConcatTest.java
+++ src/main/java/ru/job4j/concat/ConcatTest.java
@@ -12,13 +12,3 @@ public class ConcatTest {
     }
 }
 
-
-
-//    long startTime = System.currentTimeMillis();
-//    String str = "Job4j";
-//        for (int index = 0; index < 9999; index++) {
-//        str = str + index;
-//        }
-//        System.out.println("Миллисекунд: "
-//        + (System.currentTimeMillis() - startTime));
-
