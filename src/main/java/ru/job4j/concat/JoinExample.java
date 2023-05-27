ca9b46207948a6661c8de9a2e3fe635f09fab8ca
Andrey Weselov <weselov@list.ru>
2023-05-27 19:20:28 +0300

    3. Тестовое задание. Блок "Базовый синтаксис" [#505049]

--- src/main/java/ru/job4j/concat/JoinExample.java
+++ src/main/java/ru/job4j/concat/JoinExample.java
@@ -2,9 +2,9 @@ package ru.job4j.concat;
 
 public class JoinExample {
     public static void main(String[] args) {
-        String str = String.join(" * "
-                , "abc"
-                , "def", "ghi");
+        String str = String.join(" * ",
+                "abc",
+                "def", "ghi");
         System.out.println(str);
     }
 }
--- src/main/java/ru/job4j/first/Task18.java
+++ src/main/java/ru/job4j/first/Task18.java
@@ -7,11 +7,11 @@ public class Task18 {
         Scanner input = new Scanner(System.in);
         double a = input.nextDouble();
         double r = input.nextDouble();
-        double perimetr = 4*a;
-        double dlinna = 2*(Math.PI)*r;
-        double circle = Math.PI* Math.pow(r,2);
+        double perimetr = 4 * a;
+        double dlinna = 2 * (Math.PI) * r;
+        double circle = Math.PI * Math.pow(r, 2);
         String out = String.format("%.1f%n%.1f%n%.1f"
-                ,perimetr,dlinna,circle);
+                , perimetr, dlinna, circle);
         System.out.println(out);
     }
 }
